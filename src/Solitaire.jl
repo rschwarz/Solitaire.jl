b654777ef582eb6fa2fa5fd87f3811ff4b4e80c7
Robert Schwarz <mail@rschwarz.net>
2019-01-10 20:38:34 +0100

    add move_A

--- src/Solitaire.jl
+++ src/Solitaire.jl
@@ -1,3 +1,22 @@
 module Solitaire
 
+const Card = Int8
+const A = Card(53) # large joker
+const B = Card(54) # small joker
+
+const Deck = Vector{Card}
+
+"Full deck of cards (with 2 jokers), sorted in increasing order."
+sorted_deck() = Deck(1:54)
+
+"Move A one position, with wrap-around."
+function move_A(deck::Deck)
+    pos = findfirst(isequal(A), deck)
+    if pos == length(deck)
+        return vcat(deck[1], A, deck[2:end-1])
+    else
+        return vcat(deck[1:pos-1], deck[pos+1], A, deck[pos+2:end])
+    end
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,13 @@
 using Test
+using Solitaire
+
+@testset "individual moves" begin
+    Card = Solitaire.Card
+    A = Solitaire.A
+
+    @testset "move_A" begin
+        @test Solitaire.move_A(Card[A,1,2]) == Card[1,A,2]
+        @test Solitaire.move_A(Card[1,A,2]) == Card[1,2,A]
+        @test Solitaire.move_A(Card[1,2,A]) == Card[1,A,2]
+    end
+end
