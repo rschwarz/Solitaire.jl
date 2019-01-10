7c4c79fd57b924b5253bddcf9b06f33b0ee05d0d
Robert Schwarz <mail@rschwarz.net>
2019-01-10 20:45:33 +0100

    add move_B

--- src/Solitaire.jl
+++ src/Solitaire.jl
@@ -19,4 +19,16 @@ function move_A(deck::Deck)
     end
 end
 
+"Move B two positions, with wrap-around."
+function move_B(deck::Deck)
+    pos = findfirst(isequal(B), deck)
+    if pos == length(deck)
+        return vcat(deck[1:2], B, deck[3:end-1])
+    elseif pos == length(deck) - 1
+        return vcat(deck[1], B, deck[2:pos-1], deck[pos+1:end])
+    else
+        return vcat(deck[1:pos-1], deck[pos+1:pos+2], B, deck[pos+3:end])
+    end
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,18 @@ using Solitaire
 @testset "individual moves" begin
     Card = Solitaire.Card
     A = Solitaire.A
+    B = Solitaire.B
 
     @testset "move_A" begin
         @test Solitaire.move_A(Card[A,1,2]) == Card[1,A,2]
         @test Solitaire.move_A(Card[1,A,2]) == Card[1,2,A]
         @test Solitaire.move_A(Card[1,2,A]) == Card[1,A,2]
+    end
+
+    @testset "move_B" begin
+        @test Solitaire.move_B(Card[B,1,2]) == Card[1,2,B]
+        @test Solitaire.move_B(Card[1,B,2]) == Card[1,B,2]
+        @test Solitaire.move_B(Card[1,2,B]) == Card[1,2,B]
+        @test Solitaire.move_B(Card[1,2,B,3]) == Card[1,B,2,3]
     end
 end
